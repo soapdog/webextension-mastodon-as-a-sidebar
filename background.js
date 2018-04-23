afbdcbec2564d07d4aaf51c354c6477c272325f2
andre alves garzia <andre@andregarzia.com>
2018-04-23 20:29:29 -0300

    moved to pinafore

--- background.js
+++ background.js
@@ -1,15 +1,28 @@
 browser.menus.create({
   id: "reload-pinafore",
   title: "Reload Pinafore",
-  documentUrlPatterns: ["https://pinafore.social/*"] 
+  documentUrlPatterns: ["https://pinafore.social/*"]
 });
 
 function reloadSidebar() {
-  browser.sidebarAction.setPanel({panel: "https://pinafore.social"});
+  browser.sidebarAction.setPanel({ panel: "https://pinafore.social" });
 }
 
-browser.menus.onClicked.addListener(function(info, tab) {
+browser.menus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId == "reload-pinafore") {
     browser.sidebarAction.getPanel({}).then(reloadSidebar)
+  }
+});
+
+browser.commands.onCommand.addListener(function (command) {
+  if (command == "toggle-sidebar") {
+    browser.sidebarAction.isOpen({}).then(isOpen => {
+      if (isOpen) {
+        browser.sidebarAction.close()
+      } else {
+        browser.sidebarAction.open();
+      }
+    });
+
   }
 });
